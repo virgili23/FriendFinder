5a26b5c4bade18c8af474a7ae350a0caa4a4a0cc
virgil23 <virgil17@live.com>
2018-06-16 21:45:40 -0400

    Added files and worked on it

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+// This holds the friends information
+var friends = [
+    { 
+    "name": "SpongeBob",
+    "photo": "https://giphy.com/gifs/14ut8PhnIwzros/html5", 
+    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] 
+}, {
+    "name": "Patrick", 
+    "photo": 'https://giphy.com/gifs/14ut8PhnIwzros/html5', 
+    "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
+}, { 
+    "name": "Squidward", 
+    "photo": "../images/spongebob.png", 
+    "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] 
+}, { 
+    "name": "Mr Crabs",
+    "photo": "../images/mrCrabs.png", 
+    "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] 
+}, {
+    "name": "Sandy",
+    "photo": "../images/spongebob.png",
+    "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] 
+}, { 
+    "name": "Some guy",
+    "photo": "../images/spongebob.png",
+    "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] 
+}
+];
+
+
+module.exports = friends;
