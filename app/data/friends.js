ce116d1facc80fe4c80ae9c8a8e92c713505639c
virgil23 <virgil17@live.com>
2018-06-16 23:32:56 -0400

    finalized

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@ var friends = [
     { 
     "name": "SpongeBob",
     "photo": "https://giphy.com/gifs/14ut8PhnIwzros/html5", 
-    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] 
+    "scores": ["5", "1", "5", "4", "5", "1", "2", "5", "4", "1"] 
 }, {
     "name": "Patrick", 
-    "photo": 'https://giphy.com/gifs/14ut8PhnIwzros/html5', 
-    "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
+    "photo": 'https://giphy.com/gifs/J0kolcBZYnxfO/html5', 
+    "scores": ["1", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
 }, { 
     "name": "Squidward", 
-    "photo": "../images/spongebob.png", 
-    "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] 
+    "photo": "https://giphy.com/gifs/zaDi0mXkYM3eg/html5", 
+    "scores": ["3", "4", "5", "2", "1", "1", "3", "2", "5", "5"] 
 }, { 
     "name": "Mr Crabs",
-    "photo": "../images/mrCrabs.png", 
-    "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] 
+    "photo": "https://giphy.com/gifs/LdOyjZ7io5Msw/html5", 
+    "scores": ["1", "4", "4", "2", "2", "1", "3", "2", "2", "3"] 
 }, {
     "name": "Sandy",
-    "photo": "../images/spongebob.png",
-    "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] 
+    "photo": "https://giphy.com/gifs/3o7GUCZncZRz2UhJKM/html5",
+    "scores": ["3", "3", "3", "1", "5", "2", "5", "3", "1", "4"] 
 }, { 
-    "name": "Some guy",
-    "photo": "../images/spongebob.png",
-    "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] 
+    "name": "Lobster boy",
+    "photo": "https://giphy.com/gifs/F19kVYBeJkrVm/html5",
+    "scores": ["4", "3", "4", "3", "2", "2", "3", "2", "4", "5"] 
 }
 ];
 
